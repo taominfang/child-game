d5cd90941af0245eb10e69d013285b36a2f39abe
minfang tao <minfang.tao@oath.com>
2018-09-03 14:07:06 -0700

    add init

---
+++ app/config/system.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+define('__PROJECT_ROOT__', realpath(dirname(__FILE__).'/../'));
+
+define('__PROJECT_HEADER__','query-helper/');
+
+define('__PROJECT_HEADER_LENGTH__',  strlen(__PROJECT_HEADER__));
+
+define('__DEFAULT_TIME_ZONE__','America/Los_Angeles');
---
+++ app/controllers/example.php
@@ -0,0 +1,34 @@
+<?php
+
+class ExampleController extends BasicController{
+
+
+
+    public function pre_filter(&$methodName = null) {
+        parent::pre_filter($methodName);
+
+        $this->view->addInternalJs("jquery-1.7.1.min.js");
+        $this->view->addInternalJs("jquery-ui-1.8.17.custom.min.js");
+        $this->view->addInternalCss("ui-lightness/jquery-ui-1.8.17.custom.css");
+
+
+
+
+    }
+
+	public function index(){
+
+
+		$this->set('exa1','hello');
+        $this->view->exa2=array('world',' .');
+
+
+	}
+
+	public function ajax(){
+		$this->setLayout("ajax.phtml");
+		$this->set("exa3", "Hello World, ajax");
+	}
+}
+
+?>
